c1ca8341dc481400fccd759d10cfc5057f0a1228
DanteArceneaux <dantearcene@gmail.com>
2021-11-17 16:51:53 +0700

    Start of my program

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let;
+let arrayChoice = ["Rock", "Paper", "Scissors"];
+let choice = arrayChoice[Math.floor(Math.random() * arrayChoice.length)];
+
+// This is the computer's choice of Rock, Paper, Scissors.
+let computerPlay = function () {
+	return choice;
+};
+
+let singleRound(playerSelection, computerSelection) {
+
+	computerSelection = arrayChoice[Math.floor(Math.random() * arrayChoice.length)];
+
+}
+
